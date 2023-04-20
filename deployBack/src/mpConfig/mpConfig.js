1f089bae2a194c060138348373e288e90550f460
deploy-booksKingdom <131411683+deploy-booksKingdom@users.noreply.github.com>
2023-04-20 19:58:21 -0300

    first commit and first run

---
+++ deployBack/src/mpConfig/mpConfig.js
@@ -0,0 +1,9 @@
+const mercadopago = require('mercadopago');
+
+mercadopago.configure({
+    access_token: 'APP_USR-3594846635662107-041019-7bd48c43894ac0e2bd16975b12b28dbc-1349625519'
+});
+
+module.exports = {
+    mercadopago
+}
